ff6d004abba4be5a537e1aa628f492f775772a0d
Alex Peng <alexpeng014@gmail.com>
2026-01-08 23:42:20 -0500

    No longer using pool to create connection

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -3,10 +3,7 @@
 
 const { PrismaClient } = require('../dist/generated/prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg(process.env.DATABASE_URL);
 
 const prisma = new PrismaClient({ adapter });
 
